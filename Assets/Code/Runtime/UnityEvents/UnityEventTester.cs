5fc3311b410f3e9ff046374d5c5d8931342f64ad
Vheos <Vheos777@gmail.com>
2023-10-18 01:56:43 +0200

    Added method `NotifyOnShootGun`

--- Assets/Code/Runtime/UnityEvents/UnityEventTester.cs
+++ Assets/Code/Runtime/UnityEvents/UnityEventTester.cs
@@ -8,4 +8,8 @@ public class UnityEventTester : MonoBehaviour
 		=> Debug.Log($"{nameof(SwitchGun)}\n" +
 			$"\t{nameof(context.From)}: {(context.From != null ? context.From.name : "null")}\n" +
 			$"\t{nameof(context.To)}: {(context.To != null ? context.To.name : "null")}");
+
+	public void NotifyOnShootGun(ShootGun.Context context)
+		=> Debug.Log($"{nameof(ShootGun)}\n" +
+			$"\t{nameof(context.Gun)}: {(context.Gun != null ? context.Gun.name : "null")}\n");
 }
