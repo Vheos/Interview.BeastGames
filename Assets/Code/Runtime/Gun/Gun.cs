97e63ecedda2d4c1807560fb78627563b57436fa
Vheos <Vheos777@gmail.com>
2023-10-18 03:29:36 +0200

    Added bullet instantiation

--- Assets/Code/Runtime/Gun/Gun.cs
+++ Assets/Code/Runtime/Gun/Gun.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
-public class Gun : MonoBehaviour, IShooter
+public class Gun : MonoBehaviour
 {
+	[SerializeField] private GunAttributes attributes;
+	[SerializeField] private GunAnchors anchors;
+	[SerializeField] private Bullet bulletPrefab;
 
 	public void ApplyGripOffset()
 		=> transform.localPosition = -anchors.GripLocal;
 
 	public bool TryShoot()
 	{
+		Bullet bullet = Instantiate(bulletPrefab, anchors.MuzzleWorld, Quaternion.identity);
+
 		return true;
 	}
 
