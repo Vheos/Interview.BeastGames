2171b903b5cc37ea9a3e941c6303d18a0e928b38
Vheos <Vheos777@gmail.com>
2023-10-18 03:27:16 +0200

    Added component `GunAttributes`

---
+++ Assets/Code/Runtime/Gun/GunAttributes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+public class GunAttributes : MonoBehaviour
+{
+	[SerializeField, Range(0f, 10f)] private float damage = 5f;
+	[SerializeField] private DamageModifier[] overrideDamageModifiers;
+	[SerializeField, Range(0f, 2f)] private float fallbackDamageModifier = 1f;
+}
