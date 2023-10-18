bdf1c328474f858b843e27ff4fa93f7fea780b96
Vheos <Vheos777@gmail.com>
2023-10-18 17:49:56 +0200

    Updated `UnityEventTester`

--- Assets/Code/Runtime/UnityEvents/UnityEventTester.cs
+++ Assets/Code/Runtime/UnityEvents/UnityEventTester.cs
@@ -7,20 +7,27 @@ public class UnityEventTester : MonoBehaviour
 	private string NameOrNull(Object obj)
 		=> obj != null ? obj.name : "null";
 
-	public void NotifyOnSwitchGun(SwitchGun.Context context)
-		=> Debug.Log($"{Prefix}{nameof(SwitchGun)}\n" +
-			$"\t{nameof(context.From)}: {NameOrNull(context.From)}\n" +
-			$"\t{nameof(context.To)}: {NameOrNull(context.To)}");
+	public void NotifyOnAddRemoveGun(OnAddRemoveGun.Data data)
+		=> Debug.Log($"{Prefix}{nameof(OnAddRemoveGun)}\n" +
+			$"\t{nameof(data.Gun)}: {NameOrNull(data.Gun)}");
 
-	public void NotifyOnShootGun(ShootGun.Context context)
-		=> Debug.Log($"{Prefix}{nameof(ShootGun)}\n" +
-			$"\t{nameof(context.Gun)}: {NameOrNull(context.Gun)}\n");
+	public void NotifyOnSwitchGun(OnSwitchGun.Data data)
+		=> Debug.Log($"{Prefix}{nameof(OnSwitchGun)}\n" +
+			$"\t{nameof(data.From)}: {NameOrNull(data.From)}\n" +
+			$"\t{nameof(data.To)}: {NameOrNull(data.To)}");
 
-	public void NotifyOnAddGun(AddGun.Context context)
-	=> Debug.Log($"{Prefix}{nameof(AddGun)}\n" +
-		$"\t{nameof(context.Gun)}: {NameOrNull(context.Gun)}\n");
+	public void NotifyOnShootGun(OnShoot.Data data)
+		=> Debug.Log($"{Prefix}{nameof(OnShoot)}\n" +
+			$"\t{nameof(data.Gun)}: {NameOrNull(data.Gun)}\n" +
+			$"\t{nameof(data.Bullet)}: {NameOrNull(data.Bullet)}");
 
-	public void NotifyOnRemoveGun(RemoveGun.Context context)
-	=> Debug.Log($"{Prefix}{nameof(RemoveGun)}\n" +
-		$"\t{nameof(context.Gun)}: {NameOrNull(context.Gun)}\n");
+	public void NotifyOnHit(OnHit.Data data)
+		=> Debug.Log($"{Prefix}{nameof(OnHit)}\n" +
+			$"\t{nameof(data.Bullet)}: {NameOrNull(data.Bullet)}\n" +
+			$"\t{nameof(data.Collider)}: {NameOrNull(data.Collider)}");
+
+	public void NotifyOnGetShot(OnGetShot.Data data)
+		=> Debug.Log($"{Prefix}{nameof(OnGetShot)}\n" +
+			$"\t{nameof(data.Destructible)}: {NameOrNull(data.Destructible)}\n" +
+			$"\t{nameof(data.Bullet)}: {NameOrNull(data.Bullet)}");
 }
