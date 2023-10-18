b4f59f586a0a09e85065daca695c263c9a0fceaf
Vheos <Vheos777@gmail.com>
2023-10-18 04:46:27 +0200

    Updated `UnityEventTester`

--- Assets/Code/Runtime/UnityEvents/UnityEventTester.cs
+++ Assets/Code/Runtime/UnityEvents/UnityEventTester.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 public class UnityEventTester : MonoBehaviour
 {
+	private const string Prefix = "EVENT: ";
+
+	private string NameOrNull(Object obj)
+		=> obj != null ? obj.name : "null";
+
 	public void NotifyOnSwitchGun(SwitchGun.Context context)
-		=> Debug.Log($"{nameof(SwitchGun)}\n" +
-			$"\t{nameof(context.From)}: {(context.From != null ? context.From.name : "null")}\n" +
-			$"\t{nameof(context.To)}: {(context.To != null ? context.To.name : "null")}");
+		=> Debug.Log($"{Prefix}{nameof(SwitchGun)}\n" +
+			$"\t{nameof(context.From)}: {NameOrNull(context.From)}\n" +
+			$"\t{nameof(context.To)}: {NameOrNull(context.To)}");
 
 	public void NotifyOnShootGun(ShootGun.Context context)
-		=> Debug.Log($"{nameof(ShootGun)}\n" +
-			$"\t{nameof(context.Gun)}: {(context.Gun != null ? context.Gun.name : "null")}\n");
+		=> Debug.Log($"{Prefix}{nameof(ShootGun)}\n" +
+			$"\t{nameof(context.Gun)}: {NameOrNull(context.Gun)}\n");
+
+	public void NotifyOnAddGun(AddGun.Context context)
+	=> Debug.Log($"{Prefix}{nameof(AddGun)}\n" +
+		$"\t{nameof(context.Gun)}: {NameOrNull(context.Gun)}\n");
+
+	public void NotifyOnRemoveGun(RemoveGun.Context context)
+	=> Debug.Log($"{Prefix}{nameof(RemoveGun)}\n" +
+		$"\t{nameof(context.Gun)}: {NameOrNull(context.Gun)}\n");
 }
