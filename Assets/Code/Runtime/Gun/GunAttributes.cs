8ef27ae37288bc1252e710e38f1594e2c1171e70
Vheos <Vheos777@gmail.com>
2023-10-19 00:41:27 +0200

    Added methods to calculate damage by armor type

--- Assets/Code/Runtime/Gun/GunAttributes.cs
+++ Assets/Code/Runtime/Gun/GunAttributes.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GunAttributes : MonoBehaviour
 {
 	// Inspector
 	[SerializeField, Range(0f, 10f)] private float damage = 5f;
-	[SerializeField] private DamageModifier[] overrideDamageModifiers;
-	[SerializeField, Range(0f, 2f)] private float fallbackDamageModifier = 1f;
+	[SerializeField] private DamageModifier[] damageModifiers;
+	[SerializeField, Range(0f, 1f)] private float fallbackDamageModifier = 1f;
 
 	public float Damage
 		=> damage;
-	public IReadOnlyList<DamageModifier> OverrideDamageModifiers
-		=> overrideDamageModifiers;
+	public IReadOnlyList<DamageModifier> DamageModifiers
+		=> damageModifiers;
 	public float FallbackDamageModifier
 		=> fallbackDamageModifier;
+
+	public float GetDamageModifierFor(ArmorType armorType)
+	{
+		foreach (var damageModifier in damageModifiers)
+			if (damageModifier.ArmorType == armorType)
+				return damageModifier.Multiplier;
+
+		return fallbackDamageModifier;
+	}
+	public float GetDamageDealtTo(ArmorType armorType)
+		=> damage * GetDamageModifierFor(armorType);
 }
