b9312253fd2964ec3649667901964b5f33c39e3b
Vheos <Vheos777@gmail.com>
2023-10-18 03:29:22 +0200

    Added method `ApplyGripOffset`

--- Assets/Code/Runtime/Gun/Gun.cs
+++ Assets/Code/Runtime/Gun/Gun.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour, IShooter
 {
+
+	public void ApplyGripOffset()
+		=> transform.localPosition = -anchors.GripLocal;
+
 	public bool TryShoot()
 	{
 		return true;
+	}
+
+	private void Start()
+	{
+		ApplyGripOffset();
 	}
 }
