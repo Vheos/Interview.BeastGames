0f19a163568a3555a6cb4ce670ac9825068914b3
Vheos <Vheos777@gmail.com>
2023-10-18 03:28:06 +0200

    Added local position getters in `GunAnchors`

--- Assets/Code/Runtime/Gun/GunAnchors.cs
+++ Assets/Code/Runtime/Gun/GunAnchors.cs
@@ -5,8 +5,12 @@ public class GunAnchors : MonoBehaviour
 	[SerializeField] private Transform grip;
 	[SerializeField] private Transform muzzle;
 
-	public Vector3 Grip
+	public Vector3 GripLocal
+		=> grip.localPosition;
+	public Vector3 GripWorld
 		=> grip.position;
-	public Vector3 Muzzle
+	public Vector3 MuzzleLocal
+		=> muzzle.localPosition;
+	public Vector3 MuzzleWorld
 		=> muzzle.position;
 }
