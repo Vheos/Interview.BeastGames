2972a64eb2f44077ff8487e97611aedadd8e6ed0
Vheos <Vheos777@gmail.com>
2023-10-18 04:06:07 +0200

    Added public getters in `GunAttributes`

--- Assets/Code/Runtime/Gun/GunAttributes.cs
+++ Assets/Code/Runtime/Gun/GunAttributes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GunAttributes : MonoBehaviour
@@ -6,4 +7,11 @@ public class GunAttributes : MonoBehaviour
 	[SerializeField, Range(0f, 10f)] private float damage = 5f;
 	[SerializeField] private DamageModifier[] overrideDamageModifiers;
 	[SerializeField, Range(0f, 2f)] private float fallbackDamageModifier = 1f;
+
+	public float Damage
+		=> damage;
+	public IReadOnlyList<DamageModifier> OverrideDamageModifiers
+		=> overrideDamageModifiers;
+	public float FallbackDamageModifier
+		=> fallbackDamageModifier;
 }
