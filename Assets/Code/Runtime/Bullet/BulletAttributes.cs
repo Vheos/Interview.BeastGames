f0a346d616f297ad1ce21495b87dc6f767045bdb
Vheos <Vheos777@gmail.com>
2023-10-19 04:02:20 +0200

    Added inspector field `dealDamageOnHit`

--- Assets/Code/Runtime/Bullet/BulletAttributes.cs
+++ Assets/Code/Runtime/Bullet/BulletAttributes.cs
@@ -3,11 +3,14 @@
 public class BulletAttributes : MonoBehaviour
 {
 	// Inspector
-	[SerializeField, Range(1f, 100f)] private float speed = 50f;
+	[SerializeField, Range(1f, 10f)] private float force = 5f;
 	[SerializeField, Range(0f, 10)] private float lifetime = 5f;
+	[SerializeField] private bool dealDamageOnHit = true;
 
-	public float Speed
-		=> speed;
+	public float Force
+		=> force * 1000f;
 	public float Lifetime
 		=> lifetime;
+	public bool DealDamageOnHit
+		=> dealDamageOnHit;
 }
