7627c774a67faa286e7d6e0c4a2cfe4f627c9e36
Vheos <Vheos777@gmail.com>
2023-10-20 22:00:39 +0200

    Changed header consts to `nameof()`

--- Assets/Code/Runtime/Helpers/Headers.cs
+++ Assets/Code/Runtime/Helpers/Headers.cs
@@ -1,5 +1,7 @@
 ﻿public static class Headers
 {
-	public const string Dependencies = "Dependencies";
-	public const string Events = "Events";
+	public const string Dependencies = nameof(Dependencies);
+	public const string Events = nameof(Events);
+	public const string Prefabs = nameof(Prefabs);
+	public const string Values = nameof(Values);
 }
