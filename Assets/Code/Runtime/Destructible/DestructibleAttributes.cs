bbdadcd9c23b6341dea9ca8ab38b9ed6416e299a
Vheos <Vheos777@gmail.com>
2023-10-20 06:33:20 +0200

    Added field `hitParticlePrefab`

--- Assets/Code/Runtime/Destructible/DestructibleAttributes.cs
+++ Assets/Code/Runtime/Destructible/DestructibleAttributes.cs
@@ -5,9 +5,12 @@ public class DestructibleAttributes : MonoBehaviour
 	// Inspector
 	[SerializeField, Range(1f, 100f)] private float maxHealth = 50f;
 	[SerializeField] private ArmorType armorType = default;
+	[SerializeField] private HitParticle hitParticlePrefab;
 
 	public float MaxHealth
 		=> maxHealth;
 	public ArmorType ArmorType
 		=> armorType;
+	public HitParticle HitParticlePrefab
+		=> hitParticlePrefab;
 }
