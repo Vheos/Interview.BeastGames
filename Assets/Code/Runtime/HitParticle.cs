6aee345d6b0c1fea0bb7c5d466c8d35b8d27ad22
Vheos <Vheos777@gmail.com>
2023-10-20 06:26:41 +0200

    Added component `HitParticle`

---
+++ Assets/Code/Runtime/HitParticle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitParticle : MonoBehaviour
+{
+	[Header(Headers.Dependencies)]
+	[SerializeField] private new ParticleSystem particleSystem;
+	[SerializeField] private ParticleSystem decal;
+
+	public bool IsDecalVisible
+	{
+		get => decal != null && decal.gameObject.activeSelf;
+		set
+		{
+			if (decal == null)
+				return;
+
+			decal.gameObject.SetActive(value);
+		}
+	}
+}
