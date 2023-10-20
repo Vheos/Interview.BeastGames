76074314399b113676a9b095710551a5bcb2870b
Vheos <Vheos777@gmail.com>
2023-10-20 02:20:45 +0200

    Moved attribute `BulletSpread` to `Gun`

--- Assets/Code/Runtime/Bullet/BulletAttributes.cs
+++ Assets/Code/Runtime/Bullet/BulletAttributes.cs
@@ -4,15 +4,12 @@ public class BulletAttributes : MonoBehaviour
 {
 	// Inspector
 	[SerializeField, Range(0f, 10f)] private float force = 5f;
-	[SerializeField, Range(0f, 15f)] private float spread = 5f;
 	[SerializeField, Range(0f, 10f)] private float maxTime = 3f;
 	[SerializeField, Range(0f, 100)] private int maxHits = 1;
 	[SerializeField] private bool dealDamageOnCollision = true;
 
 	public float Force
 		=> force * 1000f;
-	public float Spread
-		=> spread;
 	public float MaxTime
 		=> maxTime;
 	public int MaxHits
--- Assets/Code/Runtime/Gun/GunAttributes.cs
+++ Assets/Code/Runtime/Gun/GunAttributes.cs
@@ -7,6 +7,7 @@ public class GunAttributes : MonoBehaviour
 	// Inspector
 	[SerializeField, Range(0f, 10f)] private float damage = 5f;
 	[SerializeField, Range(1, 10)] private int bulletCount = 1;
+	[SerializeField, Range(0f, 15f)] private float bulletSpread = 5f;
 	[SerializeField] private DamageModifier[] damageModifiers;
 	[SerializeField, Range(0f, 1f)] private float fallbackDamageModifier = 1f;
 
@@ -14,6 +15,8 @@ public class GunAttributes : MonoBehaviour
 		=> damage;
 	public int BulletCount
 		=> bulletCount;
+	public float BulletSpread
+		=> bulletSpread;
 	public IReadOnlyList<DamageModifier> DamageModifiers
 		=> damageModifiers;
 	public float FallbackDamageModifier
