fa68d083ff4d124a36d497fda3a69c6b047eb994
Vheos <Vheos777@gmail.com>
2023-10-20 06:33:34 +0200

    Added fields `bulletPrefab` and `recoil

--- Assets/Code/Runtime/Gun/GunAttributes.cs
+++ Assets/Code/Runtime/Gun/GunAttributes.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class GunAttributes : MonoBehaviour
 {
 	// Inspector
+	[SerializeField] private Bullet bulletPrefab;
 	[SerializeField, Range(0f, 10f)] private float damage = 5f;
 	[SerializeField, Range(1, 10)] private int bulletCount = 1;
 	[SerializeField, Range(0f, 15f)] private float bulletSpread = 5f;
+	[SerializeField, Range(0f, 30f)] private float recoil = 5f;
+
 	[SerializeField] private DamageModifier[] damageModifiers;
 	[SerializeField, Range(0f, 1f)] private float fallbackDamageModifier = 1f;
 
+	public Bullet BulletPrefab
+		=> bulletPrefab;
 	public float Damage
 		=> damage;
 	public int BulletCount
 		=> bulletCount;
 	public float BulletSpread
 		=> bulletSpread;
+	public float Recoil
+		=> recoil;
 	public IReadOnlyList<DamageModifier> DamageModifiers
 		=> damageModifiers;
 	public float FallbackDamageModifier
